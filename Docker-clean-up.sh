97e9e68db16f4b6c199faccbcd131201b1f61b87
AzRenoxM <73461703+AzRenoxM@users.noreply.github.com>
2025-05-20 15:31:44 +0200

    Update Docker-clean-up.sh
    corrected the case

--- Docker-clean-up.sh
+++ Docker-clean-up.sh
@@ -7,13 +7,13 @@ echo $line
 read -p "Do you want to delete all images[i], all stoped containers[c] or both[b]?[i, c, b]" answer
 echo $line
 case $answer in
-        i)
+        c)
                 cont_number=$(($(docker ps -a | wc -l) -1 ))
                 cont_ID=$(sudo docker ps -a | tr -s ' ' | tr -s "\t" | cut -d ' ' -f 1 | tail -$(($(sudo docker ps -a | wc -l) - 1)) | tr ' ' $'\t')  
                 for ID_number in $cont_ID; do 
                         sudo docker rm $ID_number
                 done;;
-        c)
+        i)
                 images_ID=$(sudo docker images | tail -$(($(sudo docker images | wc -l) - 1)) | tr $'\t' ' ' | tr -s ' ' | cut -d ' ' -f 3)
                 for im_ID in $images_ID; do
                         sudo docker rmi $im_ID
